1b8e4ed9eb66b5e1eed448bb5e31e2d558ae19b9
neteraxe <neteraxe@126.com>
2023-02-28 00:32:07 +0800

    change name

--- main.cpp
+++ main.cpp
@@ -5,25 +5,25 @@ using namespace std;
 
 namespace fs = std::filesystem;
 
-bool isMatch(string &&p1, string &p2)
+bool match(string &&p1, string &p2)
 {
 	return p1.find(p2) != string::npos;
 }
 
 // c plus plus has a recursive directory iterator, but it is not elegant and useless.
-void find(string &&p, string &match)
+void find(string &&p, string &pattern)
 {
 	for (auto &entry : fs::directory_iterator(p, fs::directory_options::skip_permission_denied))
 	{
 		if (!fs::is_directory(entry.path()))
 		{
-			if (isMatch(entry.path().string(), match))
+			if (match(entry.path().string(), pattern))
 			{
 				cout << entry << '\n';
 			}
 		}
 		else
-			find(entry.path().string(), match);
+			find(entry.path().string(), pattern);
 	}
 }
 
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	string match = argv[2];
+	string pattern = argv[2];
 
-	find(argv[1], match);
+	find(argv[1], pattern);
 }
